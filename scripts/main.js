d28914db17baf7ce8db1e94de3d86cad4426ba8a
fregire <fregire@mail.ru>
2018-07-20 19:09:08 +0500

    Стрелки для слайдера

--- scripts/main.js
+++ scripts/main.js
@@ -5,10 +5,29 @@ $(document).ready(function(){
 		slidesToShow: 1,
 		dots: true,
 		speed: 1000,
-		arrows: false
+		arrows: true,
+		nextArrow: "<button class='slider-arrow slider-arrow--next main-slider__arrow main-slider__arrow--next' aria-label='Следующий слайд'></button>",
+		prevArrow: "<button class='slider-arrow slider-arrow--prev main-slider__arrow main-slider__arrow--prev' aria-label='Предыдущий слайд'></button>"
 	});
 
 	$(".about__content .content__text").click(function(){
 		$(this).toggleClass("content__text--expanded");
-	})
+	});
+
+	// var isVisibleTop = function(elem){
+	// 	var coords = elem.getBoundingClientRect();
+	// 	var windowHeight = document.documentElement.clientHeight;
+
+	// 	var topVisible = coords.top > 400 && coords.top < windowHeight;
+
+	// 	return topVisible;
+	// }
+
+	// var example = document.querySelector(".projects__item");
+
+	// window.addEventListener("scroll", function(){
+	// 	if(isVisible(example)){
+	// 		alert("yes");
+	// 	}
+	// });
 });
