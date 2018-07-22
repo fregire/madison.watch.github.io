4ea4f1b29ff58295f98a492bead4c8b66e18e35d
fregire <fregire@mail.ru>
2018-07-22 22:00:17 +0500

    Исправил проекты в портфолио( картинка) + избавился от лишнего в slider.js

--- scripts/slider.js
+++ scripts/slider.js
@@ -6,24 +6,16 @@
 	var btnNext = document.querySelector(".tabs__arrow--next");
 	var btnPrev = document.querySelector(".tabs__arrow--prev");
 	var MAX_TRANSLATE_X = -((links.length - 4) * 300);
+	var MIN_TRANSLATE_X = 0;
 	var oldX = 0;
 	var shiftCoords = 0;
-	var MOUSE_SENSITY = 5;
-	var currentSlide = 0;
 
 	var clearClass = function(elems, elemClass){
 		for(var i = 0; i < elems.length; i++) {
 			elems[i].classList.remove(elemClass);
 		}
 	}
-	var splitToDigits = function(number) {
-	  var digits = [];
-	  while(number) {
-	    digits.push(number % 10);
-	    number = Math.floor(number/10);
-	  }
-	  return digits;
-	}
+
 	var onSliderMousemove = function(e){
 		var diffX = e.clientX - oldX;
 
@@ -43,8 +35,8 @@
 			shiftCoords = MAX_TRANSLATE_X;
 		}
 
-		if(shiftCoords >= 0){
-			shiftCoords = 0;
+		if(shiftCoords >= MIN_TRANSLATE_X){
+			shiftCoords = MIN_TRANSLATE_X;
 		}
 
 
@@ -56,38 +48,6 @@
 
 		this.addEventListener("mouseleave", function(){
 			document.removeEventListener("mousemove", onSliderMousemove);
-		});
-	}
-
-	var isHidden = function(elem, way){
-		if(way === 'prev'){
-			var diffCoords =  slideCoordX - viewZoneCoordX;
-		} else if(way === 'next'){
-			var diffCoords = viewZoneCoordX - slideCoordX;
-		}
-		if(diffCoords < 0){
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	var moveToSlide = function(slide, way){
-		if((shiftCoords === 0) && (way === 'right')){
-			shiftCoords -= 300;
-		} else if((shiftCoords === 0) && (way === 'left')){
-			shiftCoords += 300;
-		} else if(way === 'next'){
-			hiddenWidth = slideCoordX - viewZoneCoordX;
-			shiftCoords -= hiddenWidth;
-		} else if(way === 'prev'){
-			hiddenWidth = viewZoneCoordX - slideCoordX;
-			
-			shiftCoords += hiddenWidth;
-		}
-		anime({
-				targets: slider,
-				translateX: shiftCoords
 		});
 	}
 
@@ -104,11 +64,11 @@
 
 			this.classList.add("tabs__link--active");
 			document.querySelector(".tabs__content[data-cat='" + category + "']").classList.add("tabs__content--active");
-			// sliderWrapper.removeEventListener("mousemove", onSliderMousemove);
+			sliderWrapper.removeEventListener("mousemove", onSliderMousemove);
 
-			// setTimeout(function(){
-			// 	sliderWrapper.addEventListener("mousemove", onSliderMousemove);
-			// }, 1000);
+			setTimeout(function(){
+				sliderWrapper.addEventListener("mousemove", onSliderMousemove);
+			}, 1000);
 		});
 	}
 })();
