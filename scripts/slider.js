7000871554442dc15f7f02cba66d7f33b286be7f
fregire <fregire@mail.ru>
2018-07-24 12:00:34 +0500

    Добавил возможность слайдера если больше 5 элементов

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,11 +1,13 @@
 (function(){
-	var slider = document.querySelector(".js-category .category__items");
-	var sliderWrapper = document.querySelector(".js-category .category__inner");
-	var links = document.querySelectorAll(".js-category .category__item");
+	var slider = document.querySelector(".js-slider  .category__items");
+	var sliderWrapper = document.querySelector(".js-slider .category__inner");
+	var catItems = document.querySelectorAll(".js-tabs .category__item");
 	var contentTabs = document.querySelectorAll(".tabs__content");
 	var btnNext = document.querySelector(".tabs__arrow--next");
 	var btnPrev = document.querySelector(".tabs__arrow--prev");
-	var MAX_TRANSLATE_X = -((links.length - 4) * 300);
+	var ITEM_WIDTH = catItems[0].offsetWidth;
+	var VISIBLE_SLIDES = 5;
+	var MAX_TRANSLATE_X = -((catItems.length - VISIBLE_SLIDES) * ITEM_WIDTH);
 	var MIN_TRANSLATE_X = 0;
 	var oldX = 0;
 	var shiftCoords = 0;
@@ -50,15 +52,17 @@
 			document.removeEventListener("mousemove", onSliderMousemove);
 		});
 	}
-
-	//sliderWrapper.addEventListener("mouseenter", onSliderMouseenter);
-
-	for(var i = 0; i < links.length; i++){
-		links[i].addEventListener("click", function(){
+	
+	if(sliderWrapper){
+		sliderWrapper.addEventListener("mouseenter", onSliderMouseenter);
+	}
+	
+	for(var i = 0; i < catItems.length; i++){
+		catItems[i].addEventListener("click", function(){
 			var category = this.getAttribute("data-cat");
 
 
-			clearClass(links, 'tabs__link--active');	
+			clearClass(catItems, 'tabs__link--active');	
 			clearClass(contentTabs, 'tabs__content--active');	
 
 			this.classList.add("tabs__link--active");
