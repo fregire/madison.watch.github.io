e1555084dab1511bea190993e41e47e2a418457e
fregire <fregire@mail.ru>
2018-07-28 18:25:20 +0500

    Адаптация для планшетов Главной стр

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,16 @@ $(document).ready(function(){
 		speed: 1000,
 		arrows: true,
 		nextArrow: "<button class='slider-arrow slider-arrow--next main-slider__arrow main-slider__arrow--next' aria-label='Следующий слайд'></button>",
-		prevArrow: "<button class='slider-arrow slider-arrow--prev main-slider__arrow main-slider__arrow--prev' aria-label='Предыдущий слайд'></button>"
+		prevArrow: "<button class='slider-arrow slider-arrow--prev main-slider__arrow main-slider__arrow--prev' aria-label='Предыдущий слайд'></button>",
+		responsive: [
+					    {
+					      breakpoint: 690,
+					      settings: {
+					      	arrows: false,
+					      	fade: true,
+					      }
+					    }
+					  ]
 	});
 
 	$(".about__content .content__text").click(function(){
@@ -34,30 +43,47 @@ $(document).ready(function(){
 
 	var projects = document.querySelectorAll(".projects__item");
 	var windowHeight = screen.height;
+
+	var headerHeight = document.querySelector(".page-header").offsetHeight;
+
 	var projectPosition = '';
 
-	window.addEventListener("scroll", function(){
-		for(var i = 0; i < projects.length; i++){
-			var bottomBorder = projects[i].getBoundingClientRect().bottom;
-			var topBorder = projects[i].getBoundingClientRect().top;
+	if(windowHeight > 900){
+		window.addEventListener("scroll", function(){
+			for(var i = 0; i < projects.length; i++){
+				var bottomBorder = projects[i].getBoundingClientRect().bottom;
+				var topBorder = projects[i].getBoundingClientRect().top;
 
-			if(bottomBorder < 60){
-				projectPosition = 'before';
-			} else if(topBorder > windowHeight - 100){
-				projectPosition = 'after';
-			} else {
-				projectPosition = '';
-			}
+				if(bottomBorder < headerHeight){
+					projectPosition = 'before';
+				} else if(topBorder > windowHeight){
+					projectPosition = 'after';
+				} else {
+					projectPosition = '';
+				}
 
-			if(projectPosition === 'after'){
-				projects[i].classList.add("projects__item--out-of-range-after");
-			} else if(projectPosition === 'before'){
-				projects[i].classList.add("projects__item--out-of-range-before");
-			} else {
-				projects[i].classList.remove("projects__item--out-of-range-after", "projects__item--out-of-range-before");
+				if(projectPosition === 'after'){
+					projects[i].classList.add("projects__item--out-of-range-after");
+				} else if(projectPosition === 'before'){
+					projects[i].classList.add("projects__item--out-of-range-before");
+				} else {
+					projects[i].classList.remove("projects__item--out-of-range-after", "projects__item--out-of-range-before");
+				}
 			}
-		}
+		});
+	}
+
+
+	$(".page-nav__phone-toggler").click(function(){
+		$('.page-header .page-nav__list').toggleClass('page-nav__list--opened');
 	});
+
+	$(document).click(function(e){
+		var pageNav = $(".page-nav");
+		if(!pageNav.is(e.target) && pageNav.has(e.target).length === 0){
+			$('.page-header .page-nav__list').removeClass('page-nav__list--opened');
+		}
+	})
 
 
 	
